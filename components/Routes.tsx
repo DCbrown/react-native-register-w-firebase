90e6af3c7e08870e503337727ace427a16137061
Pegah Eizad <peizadkh@gmail.com>
2022-01-10 15:07:39 -0800

    handle login logic and reroute

--- components/Routes.tsx
+++ components/Routes.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Router, Scene } from 'react-native-router-flux'
-import { StyleSheet } from 'react-native'
-import Login from './Login'
-import Signup from './Signup'
+import React from 'react';
+import { Router, Scene } from 'react-native-router-flux';
+import { StyleSheet } from 'react-native';
+import Login from './Login';
+import Signup from './Signup';
+import HomePage from './HomePage';
 
 const Routes = () => (
-   <Router>
-      <Scene key = "root" style={styles.container}>
-         <Scene key = "login" component = {Login} title = "Login" initial = {true} />
-         <Scene key = "signup" component = {Signup} title = "Sign Up" />
-      </Scene>
-   </Router>
-)
+  <Router>
+    <Scene key="root" style={styles.container}>
+      <Scene key="login" component={Login} title="Login" initial={true} />
+      <Scene key="signup" component={Signup} title="Sign Up" />
+      <Scene key="homepage" component={HomePage} title="HomePage" />
+    </Scene>
+  </Router>
+);
 
 const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
-   },
- });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-export default Routes
+export default Routes;
